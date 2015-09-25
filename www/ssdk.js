5358936ac2db7518a3b3a7d176c13869dc91ea9d
quanzhenan <kan_hai@189.cn>
2015-09-25 16:01:56 +0800

    sss

--- www/ssdk.js
+++ www/ssdk.js
@@ -14,7 +14,7 @@ var exec = require('cordova/exec');
 
 var sharesdk = {
   share:function(value) {
-    exec(null, null, "CDVShareSDK", "share", ["aa","bb","cc","dd","eee"]);
+    exec(null, null, "ShareSDK", "share", ["aa","bb","cc","dd","eee"]);
   }
 };
 
