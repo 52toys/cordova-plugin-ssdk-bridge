4b5cd700b584dab17a8d0608050053b4bf0d0869
quanzhenan <kan_hai@189.cn>
2015-09-25 16:48:15 +0800

    v0.1.0

--- src/ios/CDVShareSDK.m
+++ src/ios/CDVShareSDK.m
@@ -15,15 +15,74 @@
 
 -(void)share:(CDVInvokedUrlCommand *)command
 {
+    NSLog(@"CDVShareSDK->share");
+    
     int platform = [[command.arguments objectAtIndex:0] intValue];
 
     NSString* title=[command.arguments objectAtIndex:1];
     NSString* content=[command.arguments objectAtIndex:2];
     NSString* url=[command.arguments objectAtIndex:3];
-    NSString* imagePath=[command.arguments objectAtIndex:4];
 
     //platform   新浪微博 = 1, 腾讯微博 = 2, 微信好友 ＝ 22 ，微信朋友圈 ＝ 23
-    NSLog(@"%@",command);
+    NSLog(@"%@",command.arguments);
+    NSLog(@"%i",platform);
+    NSLog(@"%@",title);
+    
+    [self.commandDelegate runInBackground:^{
+        NSString* payload = nil;
+        // Some blocking logic...
+        NSLog(@"thread is running...");
+        NSURL* aURL = [NSURL URLWithString:url];
+        NSData* data = [[NSData alloc] initWithContentsOfURL:aURL];
+        
+        
+        NSArray* imageArray = @[[UIImage imageWithData:data]];
+        
+        //创建分享参数
+        NSMutableDictionary *shareParams = [NSMutableDictionary dictionary];
+        [shareParams SSDKSetupShareParamsByText:content
+                                         images:imageArray
+                                            url:[NSURL URLWithString:@"http://www.52toys.com"]
+                                          title:title
+                                           type:SSDKContentTypeAuto];
+
+        
+        [ShareSDK share:platform
+             parameters:shareParams
+         onStateChanged:^(SSDKResponseState state, NSDictionary *userData, SSDKContentEntity *contentEntity, NSError *error) {
+             
+             switch (state) {
+                 case SSDKResponseStateSuccess:
+                 {
+                     UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"分享成功"
+                                                                         message:nil
+                                                                        delegate:nil
+                                                               cancelButtonTitle:@"确定"
+                                                               otherButtonTitles:nil];
+                     [alertView show];
+                     break;
+                 }
+                 case SSDKResponseStateFail:
+                 {
+                     UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"分享失败"
+                                                                         message:[NSString stringWithFormat:@"%@", error]
+                                                                        delegate:nil
+                                                               cancelButtonTitle:@"确定"
+                                                               otherButtonTitles:nil];
+                     [alertView show];
+                     break;
+                 }
+                 default:
+                 break;
+             }
+             
+         }];
+        
+        CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:payload];
+        // The sendPluginResult method is thread-safe.
+        [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+    }];
+    
 
 }
 @end
--- www/ssdk.js
+++ www/ssdk.js
@@ -3,10 +3,14 @@
  * @param1 function(winParam) {}: A success callback function. Assuming your exec call completes successfully, this function executes along with any parameters you pass to it.
  * @param2 function(error) {}: An error callback function. If the operation does not complete successfully, this function executes with an optional error parameter.
  *
+ * @param3 plugin name
  *
- *
- *
- *
+ * @param4 objective function
+ * @param5   args  :
+ *    platformid 新浪微博 = 1, 腾讯微博 = 2, 微信好友 ＝ 22 ，微信朋友圈 ＝ 23
+ *    title
+ *    content
+ *    image url
  * **/
 
 
@@ -14,7 +18,13 @@ var exec = require('cordova/exec');
 
 var sharesdk = {
   share:function(value) {
-    exec(null, null, "ShareSDK", "share", ["aa","bb","cc","dd","eee"]);
+    var args = [];
+    for(var i=0;i<arguments.length;i++)
+    {
+      args.push(arguments[i]);
+      alert(arguments[i]);
+    }
+    exec(null, null, "ShareSDK", "share", args);
   }
 };
 
