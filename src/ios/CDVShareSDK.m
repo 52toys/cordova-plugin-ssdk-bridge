c5732aef578f5cda0e5405c9489cdd2dbc175511
quanzhenan <kan_hai@189.cn>
2015-09-25 10:48:54 +0800

    aaa

--- src/ios/CDVShareSDK.m
+++ src/ios/CDVShareSDK.m
@@ -15,12 +15,14 @@
 
 -(void)share:(CDVInvokedUrlCommand *)command
 {
-    NSString* title=[command.arguments objectAtIndex:0];
-    NSString* content=[command.arguments objectAtIndex:1];
-    NSString* url=[command.arguments objectAtIndex:2];
-    NSString* imagePath=[command.arguments objectAtIndex:3];
-    int platform = [[command.arguments objectAtIndex:4] intValue];
-//platform   新浪微博 = 1, 腾讯微博 = 2, 微信好友 ＝ 22 ，微信朋友圈 ＝ 23
+    int platform = [[command.arguments objectAtIndex:0] intValue];
+
+    NSString* title=[command.arguments objectAtIndex:1];
+    NSString* content=[command.arguments objectAtIndex:2];
+    NSString* url=[command.arguments objectAtIndex:3];
+    NSString* imagePath=[command.arguments objectAtIndex:4];
+
+    //platform   新浪微博 = 1, 腾讯微博 = 2, 微信好友 ＝ 22 ，微信朋友圈 ＝ 23
     NSLog(@"%@",command);
 
 }
